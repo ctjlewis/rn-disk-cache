1c037e5d8b23f5427c3013c8e1e2ef685e8b687a
C. Lewis <ctj.lewis@icloud.com>
2021-07-08 15:03:58 -0500

    chore: build with new TSDX

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,4 +1,4 @@
 
-export { default } from './rn-disk-cache.mjs';
-export * from './rn-disk-cache.mjs';
+export { default } from './rn-disk-cache.min.mjs';
+export * from './rn-disk-cache.min.mjs';
   
